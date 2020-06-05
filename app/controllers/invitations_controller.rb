e9640e02fc28e4e9c1a226259c5a3b4853a9fc4d
Daniel Santander <danyvo1d@gmail.com>
2020-06-05 17:36:21 -0500

    Fix stickler issues

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,16 +1,14 @@
 class InvitationsController < ApplicationController
   def index
     @invitations = Invitation.all
-
   end
 
   def new
     @invitation = Invitation.new
     @users = User.all
     @events = Event.all
-    
   end
-
+  
   def create
     @invitation = Invitation.new(invitation_params)
     @event = @invitation.attended_event_id
--- db/schema.rb
+++ db/schema.rb
@@ -12,27 +12,26 @@
 
 ActiveRecord::Schema.define(version: 2020_06_01_210640) do
 
-  create_table "events", force: :cascade do |t|
-    t.string "title", default: "", null: false
-    t.date "event_date", null: false
-    t.integer "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_events_on_user_id"
+  create_table 'events', force: :cascade do |t|
+    t.string 'title', default: '', null: false
+    t.date 'event_date', null: false
+    t.integer 'user_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ["user_id"], name: 'index_events_on_user_id'
   end
 
-  create_table "invitations", force: :cascade do |t|
-    t.integer "invitee_id"
-    t.integer "attended_event_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'invitations', force: :cascade do |t|
+    t.integer 'invitee_id'
+    t.integer 'attended_event_id'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "name", default: "", null: false
-    t.string "email", default: "", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'users', force: :cascade do |t|
+    t.string 'name', default: '', null: false
+    t.string 'email', default: '', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
-
 end
